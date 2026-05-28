f6fcf5b83ff2f39d8520d2ffadf0b9069817d9f3
darkchsq <darkchsq@naver.com>
2026-05-28 15:50:15 +0900

    [feature] Add SimilarityChecker::CalcLengthScore function - Add UnitTest for SimilarityChecker::CalcLengthScore function.

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,4 +1,12 @@
+#include <string>
+
+using std::string;
+
 class SimilarityChecker {
 public:
+	SimilarityChecker() = default;
 
+	int CalcLengthScore(const string& str1, const string& str2) {
+		return 0;
+	}
 };
--- SimilarityChecker/SimilarityChecker_Test.cpp
+++ SimilarityChecker/SimilarityChecker_Test.cpp
@@ -1,6 +1,18 @@
 #include "gmock/gmock.h"
 #include "SimilarityChecker.cpp"
 
-TEST(SimilarityChecker, TC1) {
-	EXPECT_EQ(0, 0);
+using namespace testing;
+
+class SimilarityCheckerFixture : public Test {
+public:
+	SimilarityChecker _similarity_checker;
+	void check_calc_length_score(string str1, string str2) {
+		int score = _similarity_checker.CalcLengthScore(str1, str2);
+		int expect = 0;
+		EXPECT_EQ(expect, score);
+	}
+};
+
+TEST_F(SimilarityCheckerFixture, CheckStringLength) {
+	check_calc_length_score("", "");
 }
