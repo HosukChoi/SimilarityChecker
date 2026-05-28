7c830c5b79e52ab89d7a57f82b7289d20c2e5add
darkchsq <darkchsq@naver.com>
2026-05-28 15:51:39 +0900

    [refactoring] Update UnitTest code - Get the expect value in SimilarityCheckerFixture::check_calc_length_score function

--- SimilarityChecker/SimilarityChecker_Test.cpp
+++ SimilarityChecker/SimilarityChecker_Test.cpp
@@ -6,13 +6,12 @@ using namespace testing;
 class SimilarityCheckerFixture : public Test {
 public:
 	SimilarityChecker _similarity_checker;
-	void check_calc_length_score(string str1, string str2) {
+	void check_calc_length_score(int expect, string str1, string str2) {
 		int score = _similarity_checker.CalcLengthScore(str1, str2);
-		int expect = 0;
 		EXPECT_EQ(expect, score);
 	}
 };
 
 TEST_F(SimilarityCheckerFixture, CheckStringLength) {
-	check_calc_length_score("", "");
+	check_calc_length_score(0, "", "");
 }
